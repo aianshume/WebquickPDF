5be6f5d3a5e3486ad7ef1a5c3aeff391170065ea
Ai Anshu <anshumeena1947@gmail.com>
2023-04-15 09:49:58 +0530

    add: login notfication

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,22 +9,50 @@ import {
 import { useInputState } from '@mantine/hooks';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import {auth} from "services/firebase"
+import { notifications } from "@mantine/notifications";
+import { IconCross } from "@tabler/icons-react";
 
 export default function LoginPage() {
   const [emailValue, setEmailValue] = useInputState<string>("")
   const [passwordValue, setPasswordValue] = useInputState<string>("")
 
   const submitForm = () => {
+    notifications.show({
+      id: 'login-data',
+      loading: true,
+      title: 'Logining',
+      message: 'retriving data from server please wait.',
+      autoClose: false,
+      withCloseButton: false,
+    });
     signInWithEmailAndPassword(auth, emailValue, passwordValue)
-    .then(() => {
+    .then((userCredential) => {
+      let user = userCredential.user;
+      console.log(user);
       window.localStorage.setItem("user", JSON.stringify({
         login: true
       }))
+      notifications.update({
+        id: 'login-data',
+        color: 'red',
+        title: `Welcome ${user.displayName}`,
+        message: "login successful",
+        icon: <IconCross size="1rem" />,
+        autoClose: 4000,
+      });
       // ...
     })
     .catch((error) => {
       const errorMessage = error.message;
       console.log("errorMsg", errorMessage);
+      notifications.update({
+        id: 'login-data',
+        color: 'red',
+        title: 'Error',
+        message: errorMessage,
+        icon: <IconCross size="1rem" />,
+        autoClose: 4000,
+      });
     });
   }
 
