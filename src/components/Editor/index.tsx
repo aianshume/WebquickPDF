9cd36ad081ab70ccfd193fca595b6ff3ef483b0d
Ai Anshu <anshumeena1947@gmail.com>
2023-04-18 09:26:40 +0530

    update: editor

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,65 +1,7 @@
-import { useEffect, useRef } from "react";
-// @ts-ignore
-import grapesjs from "grapesjs";
-import 'grapesjs/dist/css/grapes.min.css'
-import gjsBasicBlock from "grapesjs-blocks-basic"
-import basicCustomPlugin from './plugins/basic'
-import "styles/designer.css"
+import {PaperEditor} from "./PaperEditor"
+import {TemplateEditor} from "./TemplateEditor"
 
-interface GrapesJSProps {
-  id: string;
-  config?: any;
-  onSave?: (html: string) => void;
-  canvasSize: {
-    height: number,
-    width: number
-  }
+export {
+  PaperEditor,
+  TemplateEditor
 }
-
-function GrapesJS({ id, config, onSave, canvasSize }: GrapesJSProps) {
-  const editorRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const editor = grapesjs.init({
-      container: `#${id}`,
-      ...config,
-      deviceManager:{
-        devices: [
-          {
-            id: 'paper',
-            name: 'paper',
-            width: `${canvasSize?.width}px`,
-            height: `${canvasSize?.height}px`,
-          }
-        ],
-      },
-      storageManager: false,
-      plugins: [gjsBasicBlock, basicCustomPlugin],
-    });
-
-    editor.Panels.addButton('options', {
-      id: 'save',
-      className: 'fa fa-floppy-o',
-      command: 'save',
-      attributes: { title: 'Save' },
-      category: 'Custom Category', // add a new category for the custom icon
-    });
-
-    if (onSave) {
-      editor.Commands.add("save", {
-        run: () => {
-          onSave(`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><style>${editor.getCss()}
-</style></head><body>${editor.getHtml()}</body></html>`);
-        },
-      });
-    }
-
-    return () => {
-      editor.destroy();
-    };
-  }, [id, config, onSave]);
-
-  return <div ref={editorRef} id={id} />;
-}
-
-export default GrapesJS;
